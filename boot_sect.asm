9c59ef1c83c0e682d3dc7a538760931500c455ad
pranav142 <pknadimp@gmail.com>
2024-02-04 18:55:39 -0700

    (feat): Displaying Hello World on boot

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,5 +1,30 @@
-loop:
-    jmp loop
+mov ah, 0x0e ; Specifies interrupt function (write character to screen)
+
+mov al, 'H' ; Specifies Character to be Written
+int 0x10 ; interrupt  0x10 is a BIOS ISR related to screen functions
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+mov al, ' '
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10
+
+jmp $ ; loops forever program jumps to current instruction
 
 times 510-($-$$) db 0
 dw 0xaa55
--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 # OS From Scratch
 
+Compile boot sector to binary
 ```bash
 nasm -f bin boot_sect.asm -o boot_sect.bin
 ```
 
+Emulate boot sector in qemu
 ```bash
 qemu-system-x86_64 -drive file=boot_sect.bin,format=raw 
 ```
